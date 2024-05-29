1f815ac2fc6853fa64c203ba07d4340bffe648f6
Mübarek ALBAYATI <mubapc1@gmail.com>
2024-05-29 20:32:10 +0300

    Cari

--- MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/DepartmanController.cs
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/DepartmanController.cs
@@ -61,5 +61,13 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             ViewBag.d = dpt;
             return View(degerler);
         }
+
+        public ActionResult DepartmanPersonelSatis(int id)
+        {
+            var degerler=c.SatisHarekets.Where(x=>x.Personelid==id).ToList();
+            var per = c.Personels.Where(x => x.Personelid == id).Select(y => y.PersonelAd + " " + y.PersonelSoyad).FirstOrDefault();
+            ViewBag.dpers = per;
+            return View(degerler);
+        }
     }
 }
--- MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -10,14 +10,15 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
     public class Cariler
     {
         [Key]
-        public  int Cariid { get; set; }
+        public int Cariid { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(30, ErrorMessage = "En fazla 30 karakter yazabilirsiniz")]
+        public string CariAd { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
-        public  string CariAd { get; set; }
-
-        [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [Required(ErrorMessage = "Bu alanı boş geçemezsiniz!")]
         public string CariSoyad { get; set; }
 
         [Column(TypeName = "Varchar")]
@@ -27,6 +28,8 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [Column(TypeName = "Varchar")]
         [StringLength(50)]
         public string CariMail { get; set; }
+
+        public bool Durum { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
     }
 }
