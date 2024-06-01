060fa4a39f44790d495421ede5485797ab0f3c0e
Mübarek ALBAYATI <mubapc1@gmail.com>
2024-06-01 19:51:20 +0300

    Satışlar

--- MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -73,5 +73,11 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult UrunListesi()
+        {
+            var degerler = c.Uruns.ToList();
+            return View(degerler);
+        }
     }
 }
