eee137501c0998130da3baa4d8b7d0a700262285
Mübarek ALBAYATI <mubapc1@gmail.com>
2024-07-14 21:57:57 +0300

    Login

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class CariPanelController : Controller
+    {
+        // GET: CariPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Models/Siniflar/Personel.cs
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Models/Siniflar/Personel.cs
@@ -10,22 +10,26 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
     public class Personel
     {
         [Key]
-        public  int Personelid { get; set; }
+        public int Personelid { get; set; }
 
+
+        [Display(Name = "Personel Adı")]
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string PersonelAd { get; set; }
 
+        [Display(Name = "Personel Soyadı")]
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
         public string PersonelSoyad { get; set; }
 
+        [Display(Name = "Görsel")]
         [Column(TypeName = "Varchar")]
         [StringLength(250)]
         public string PersonelGorsel { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
 
-        public  int Departmanid { get; set; }
+        public int Departmanid { get; set; }
         public virtual Departman Departman { get; set; }
     }
 }
